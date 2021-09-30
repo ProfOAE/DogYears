b57f1d1025a68928e2503f81902774231222e88d
ProfOAE <oheneadjeie@gmail.com>
2021-09-30 19:36:16 +0000

    Create DogYears.js
    Calculating Dog Years

---
+++ DogYears.js
@@ -0,0 +1,28 @@
+//my age in years
+var myAge = 8;
+
+//first two years
+let earlyYears = 2;
+earlyYears *=10.5;
+
+// years left after calculating the first two years
+let laterYears = myAge - 2;
+
+// later age converted to dogs years
+laterYears *= 4;
+
+ var myAgeInDogYears = earlyYears+laterYears;
+var myName = 'Prof. Ohene Adjei Emmanuel';
+
+// converting my name to lower case letters
+myName = myName.toLowerCase();
+
+// displaying my name, age and age in dog years
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is 
+${myAgeInDogYears} years old in dog years\n\n`);
+
+// proportion for conversion
+const dogAge = 5.625;
+let yourAge = 26
+yourAge *=dogAge;  
+console.log(yourAge);
